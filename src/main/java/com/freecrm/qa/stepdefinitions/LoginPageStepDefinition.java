b27ab14c9b451cfeb57b6242096fb376ef62c50f
iqasim <niit.imran@gmail.com>
2018-09-10 09:14:43 +0530

    TestRunnerClassName

--- src/main/java/com/freecrm/qa/stepdefinitions/LoginPageStepDefinition.java
+++ src/main/java/com/freecrm/qa/stepdefinitions/LoginPageStepDefinition.java
@@ -1,21 +1,27 @@
 package com.freecrm.qa.stepdefinitions;
 
+import org.openqa.selenium.By;
+import org.testng.Assert;
+
+import com.freecrm.qa.base.TestBase;
+
 import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
-public class LoginPageStepDefinition {
+public class LoginPageStepDefinition extends TestBase{
 	
 	@Given("^I am on the Login page URL \"([^\"]*)\"$")
 	public void i_am_on_the_Login_page_URL(String arg1) throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
+	    driver.get(arg1);
 	    throw new PendingException();
 	}
 
 	@Then("^I should see Log In Page$")
 	public void i_should_see_Log_In_Page() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
+		boolean btnLogin = driver.findElement(By.xpath("//input[@value='Login']")).isDisplayed();
+	    Assert.assertTrue(btnLogin, "Login button is not displayed");
 	    throw new PendingException();
 	}
 
